cec49b6292b6add3bb9843c65038c274bc74773d
Milica Radumilo <radumilomilica@gmail.com>
2022-04-13 17:24:23 +0200

    Add conditionals

--- conditionals.js
+++ conditionals.js
@@ -2,22 +2,22 @@
 
 let year = 2022;
 
-if (year === 2022) {
-    console.log("It's 2022!");
+if (year === 2022){
+    console.log("It`s 2022!");
 }
 
-if (year === 2021) {
-    console.log("It's year 2021!");
+if(year === 2021){
+    console.log("It`s year 2021!");
 } else {
-    console.log("It's NOT year 2021!");
+    console.log("It`s NOT year 2021!");
 }
 
-if (year > 2022) {
-    console.log("It's after 2022!");
+if(year > 2022) {
+    console.log("It`s after 2022!");
 } else if (year < 2022) {
-    console.log("It's before 2022!");
+    console.log("It`s before 2022!");
 } else {
-    console.log("It's 2022!");
+    console.log("It`s 2022!");
 }
 
 let canDrive;
@@ -28,9 +28,8 @@ if (age >= 16) {
 } else {
     canDrive = false;
 }
-
 console.log("canDrive =", canDrive);
 
 // promenljiva = uslov ? vrednost ako je uslov tacan : vrednost ako je uslov netacan;
 let canDrive2 = (age >= 18) ? "Moze da vozi" : "Ne moze da vozi";
-console.log("canDrive2 =", canDrive2);
+console.log("canDrive2=", canDrive2);

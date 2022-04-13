843e019627d65b5396941005a46855071188e61b
Milica Radumilo <radumilomilica@gmail.com>
2022-04-13 16:41:12 +0200

    Add  comparison examples

--- comparisons.js
+++ comparisons.js
@@ -13,13 +13,14 @@ console.log("5 > 3:", result);
 console.log("2 > a", "2" > "a");
 console.log("Bee > Be", "Bee" > "Be");
 
-console.log('"2" > 1', "2" > 1);
-console.log('"01" == 1', "01" == 1);
+console.log(`"2" > 1`, "2" > 1);
+console.log(`"01" == 1`, "01" == 1);
 
 console.log("false == 0", false == 0);
 console.log("false === 0", false === 0);
-console.log('"01" === 1', "01" === 1);
-console.log("false !== 0", false !== 0);
-console.log("false != 0", false != 0);
+console.log(`"01" === 1`, "01" === 1); // strogo poredi string i broj i zato je false
+console.log("false !== 0", false !== 0); // strogo poredi boolean i broj i zato je true
+console.log("false != 0", false != 0); 
 console.log("2a" > 1);
+console.log(Number("2a"));
 console.log(NaN > 1);

c986af2c2013bf178f4747e14235a0c9fa7d9111
Milica Radumilo <radumilomilica@gmail.com>
2022-04-13 09:57:00 +0200

    update

---
+++ comparisons.js
@@ -0,0 +1,25 @@
+"use strict";
+
+console.log("2 > 1:", 2 > 1);
+console.log("2 < 1:", 2 < 1);
+console.log("2 >= 1:", 2 >= 1);
+console.log("2 <= 1:", 2 <= 1);
+console.log("2 == 1:", 2 == 1);
+console.log("2 != 1", 2 != 1);
+
+let result = 5 > 3;
+console.log("5 > 3:", result);
+
+console.log("2 > a", "2" > "a");
+console.log("Bee > Be", "Bee" > "Be");
+
+console.log('"2" > 1', "2" > 1);
+console.log('"01" == 1', "01" == 1);
+
+console.log("false == 0", false == 0);
+console.log("false === 0", false === 0);
+console.log('"01" === 1', "01" === 1);
+console.log("false !== 0", false !== 0);
+console.log("false != 0", false != 0);
+console.log("2a" > 1);
+console.log(NaN > 1);
---
+++ logic.js
@@ -0,0 +1,37 @@
+"use strict";
+
+console.log("Logical operation: OR ||");
+console.log("true || true:", true || true);
+console.log("true || false:", true || false);
+console.log("false || true:", false || true);
+console.log("false || false:", false || false);
+
+let hour = 57;
+if (hour < 0 || hour > 23) {
+    console.log("Vreme nije validno!");
+    process.exit(0);
+}
+
+if (hour < 8 || hour > 16) {
+    console.log("Kancelarija je zatvorena!");
+}
+
+console.log("Logical operation: AND &&");
+console.log("true && true:", true && true);
+console.log("true && false:", true && false);
+console.log("false && true:", false && true);
+console.log("false && false:", false && false);
+
+if (!(hour >= 8 && hour <= 16)) {
+    console.log("Kancelarija je otvorena!");
+}
+
+if ((hour >= 16 && hour <= 23) || (hour >= 0 && hour <= 8)) {
+    console.log("Kancelarija je otvorena u 3. smeni!");
+}
+
+console.log("Logical operator: NOT !");
+console.log("!true", !true);
+console.log("!false", !false);
+
+console.log("Logical operator precedence: NOT (!), AND (&&), OR (||)");

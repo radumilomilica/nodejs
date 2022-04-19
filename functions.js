6d16f93df06dfcd380683139e84d685c1bec3166
Milica Radumilo <radumilomilica@gmail.com>
2022-04-19 13:46:09 +0200

    Add functions

---
+++ functions.js
@@ -0,0 +1,32 @@
+"use strict";
+
+let additionResult = addNumber(2, 2);
+console.log("addNumber(2, 2) = ", additionResult);
+additionResult = addNumber(3, 3);
+console.log("addNumber(3, 3) = ", additionResult);
+console.log("addNumber(4, 4) = ", addNumber(4, 4));
+
+let callResult = callFunction();
+callFunction();
+
+console.log("callResult =", callResult);
+
+function callFunction() {
+    console.log("addNumber(5, 5) =", addNumber(5, 5));
+    console.log("Punoletan 18? =", checkAge(18));
+    console.log("Punoletan 12? =", checkAge(12));
+}
+
+function checkAge(age) {
+    if (age >= 18) {
+        return true;
+    }
+    return false;
+}
+    
+
+function addNumber(number1, number2) {
+    let result = number1 + number2;
+
+    return result;
+}
